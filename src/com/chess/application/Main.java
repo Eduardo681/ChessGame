b27e0a01fcecc75b7e4bfb695addb677e4a3cdc4
Eduardo Henrique da Silva <edu.slhenrique@gmail.com>
2021-07-31 14:08:19 -0300

    # Starting to implement class Board and Piece

--- src/com/chess/application/Main.java
+++ src/com/chess/application/Main.java
@@ -1,10 +1,10 @@
 package com.chess.application;
 
+import com.chess.boardgame.Board;
 import com.chess.boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
     }
 }
