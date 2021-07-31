427cbd31d5c2b18e04c7f89f9b6622c8a4906428
Eduardo Henrique da Silva <edu.slhenrique@gmail.com>
2021-07-31 18:03:22 -0300

    # CheckMate logic

--- src/com/edu/application/Main.java
+++ src/com/edu/application/Main.java
@@ -15,7 +15,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-        while (true) {
+        while (!chessMatch.isCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -40,5 +40,7 @@ public class Main {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
