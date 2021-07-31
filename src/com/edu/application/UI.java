18fd59ae901fcb7fbe9c13bf99dbc0cece69198b
Eduardo Henrique da Silva <edu.slhenrique@gmail.com>
2021-07-31 16:09:58 -0300

    # Moving pieces

--- src/com/edu/application/UI.java
+++ src/com/edu/application/UI.java
@@ -1,7 +1,11 @@
 package com.edu.application;
 
 import com.edu.chess.ChessPiece;
+import com.edu.chess.ChessPosition;
 import com.edu.chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -26,6 +30,17 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values a1:h8");
+        }
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -48,6 +63,6 @@ public class UI {
             }
         }
         System.out.print(" ");
-
     }
+
 }
